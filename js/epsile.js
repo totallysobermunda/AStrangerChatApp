bc2258317eb40650358879a806c704a9886f8931
dheeraj932 <dheerajkumar.1379@gmail.com>
2021-07-17 19:42:46 +0530

    Blog

--- js/epsile.js
+++ js/epsile.js
@@ -389,3 +389,15 @@ function redirect() {
         document.location = 'https://akadbkad.com'
     }
 }
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("main").style.marginLeft = "100%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
